ca9c9a3dd8a9854004168cd25ecf56278f6a18c2
Tim Montague <tim.john.montague@gmail.com>
2023-07-22 23:13:56 -0400

    No extra credit this time

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIRCLE_OBJECTS
+{
+    public class Circle
+    {
+        //properties
+        public double Radius { get; set; }
+
+        //constructor
+        public  Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+       //methods
+       public double CalculateDiameter()
+        {
+            return Radius * 2;
+        }
+        public double CalculateCircumference()
+        {
+            return Math.PI * Radius * 2;
+        }
+            public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+            public void Grow()
+        {
+            Radius=Radius * 2;
+        }
+            public double GetRadius()
+        {
+            return (double) Radius;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using CIRCLE_OBJECTS;
+Circle myCircle = new Circle(1);
+Console.WriteLine("Welcome to the Circle Tester");
+
+//Gets the users radius 
+while (true)
+{
+    Console.Write("\nEnter radius: ");
+    try
+    {
+        myCircle.Radius = double.Parse(Console.ReadLine().Trim());
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("\n"+ex);
+        Console.WriteLine("\nPlesetry agan");
+    }
+}
+Console.WriteLine();
+
+//Displays the circles information 
+bool run = true;
+while (run)
+{
+    Console.WriteLine("{0,10}| {1,10}| {2,15}| {3,15}|", "Radius", "Diameter", "Circumference", "Area");
+    Console.WriteLine("{0,10:n}| {1,10:n}| {2,15:n}| {3,15:n}|",
+        myCircle.GetRadius(), myCircle.CalculateDiameter(), myCircle.CalculateCircumference(), myCircle.CalculateArea());
+
+
+    //Gatekeeper 
+    //Asks the user if he or she wishes to end the program or it will double the size the
+    //radius then print the radius, diameter, circumference, and area.
+    while (true)
+    {
+        Console.Write("\nShould the circle grow? (y/n): ");
+        string entry = Console.ReadLine();
+        if (entry == "y" || entry == "n")
+        {
+            if (entry == "y")
+            {
+                myCircle.Grow();
+                break;
+            }
+            else
+            {
+                run = false;
+                break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid response ");
+        }
+    }
+}
